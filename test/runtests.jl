b15fca67a8d341d638b4fb841cfaac8277615249
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-10-21 17:38:12 +0100

    add Tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,49 @@
 using Tricks
+using InteractiveUtils
 using Test
 
-@testset "Tricks.jl" begin
-    # Write your own tests here.
+struct Iterable end; struct NonIterable end;
+iterableness_static(::Type{T}) where T = static_hasmethod(iterate, Tuple{T}) ? Iterable() : NonIterable()
+
+struct Foo end
+
+@testset "static_hasmethod" begin
+    @testset "positive" for data in (
+        "abc", [1,2,3], (2,3), ones(4,10,2), 'a',  1:100
+    )
+        T = typeof(data)
+        @test iterableness_static(T) === Iterable()
+        code_typed_ir = (@code_typed iterableness_static(T))[1].code
+        @test code_typed_ir == [:(return $(QuoteNode(Iterable())))]
+    end
+
+    @testset "negative" for data in (
+        :a, rand, Int
+    )
+        T = typeof(data)
+        @test iterableness_static(T) === NonIterable()
+        code_typed = (@code_typed iterableness_static(T))
+        @test code_typed[2] === NonIterable  # return type
+        @test length(code_typed[1].code) <= 3  # currently has dead conditions on statements 1 and 2, but LLVM removed them.
+    end
+
+    @testset "add method" begin
+        @test iterableness_static(Foo) === NonIterable()
+
+        Base.iterate(::Foo) = ("Foo", nothing);
+        Base.iterate(::Foo, ::Nothing) = nothing;
+        Base.length(::Foo) = 1;
+        @test collect(Foo()) == ["Foo"]
+
+        @test iterableness_static(Foo) === Iterable()
+    end
+
+    @testset "delete method" begin
+        @test iterableness_static(Foo) === Iterable()
+        meth = first(methods(iterate, Tuple{Foo}))
+        Base.delete_method(meth)
+        @test_throws MethodError collect(Foo())
+
+        @test_broken iterableness_static(Foo) === NonIterable()
+    end
 end
